427443b76a47ffdbfb1f57d1c0761b21890537fe
SteijlenGLR <steijlen@glr.nl>
2024-04-10 13:38:38 +0200

    db.php: Commentaar toegevoegd om de documentatie van de file te verbeteren.

--- db.php
+++ db.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * Database Connection Configuration and Initialization
+ *
+ * This script sets up a PDO database connection based on the SQLite database file
+ * specified in the .env file. It also configures PDO error handling.
+ */
+
 // Set strict types
 declare(strict_types=1);
 
@@ -14,9 +21,13 @@ $database_file = $_ENV['DB_FILE'];
 
 // PDO Database Connection
 try {
+    // Establish a connection to the SQLite database
     $db = new PDO("sqlite:$database_file");
+
+    // Set the error mode to throw exceptions
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
+    // Handle connection errors
     echo "Connection failed: " . $e->getMessage();
     exit;
 }
